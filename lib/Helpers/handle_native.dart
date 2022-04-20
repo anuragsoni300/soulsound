2018fb687968b38cd0fe5ae066dddcf7e4009513
Anurag Soni <anurag.soni@indiamart.com>
2022-04-21 00:36:17 +0530

    first commit

---
+++ lib/Helpers/handle_native.dart
@@ -0,0 +1,29 @@
+
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class NativeMethod {
+  static const MethodChannel sharedTextChannel =
+      MethodChannel('com.shadow.sharedTextChannel');
+  static const MethodChannel registermediaChannel =
+      MethodChannel('com.shadow.registerMedia');
+  static const MethodChannel intentChannel =
+      MethodChannel('com.shadow.intentChannel');
+
+  static Future<void> handleIntent() async {
+    // final _intent = await sharedTextChannel.invokeMethod('getSharedText');
+    // if (_intent != null) {
+    //   print('IntentHandler: Result: $_intent');
+    // } else {
+    //   print('intent is null');
+    // }
+  }
+
+  static Future<void> handleAudioIntent(String audioPath) async {
+    if (await File(audioPath).exists()) {
+      intentChannel.invokeMethod('openAudio', {'audioPath': audioPath});
+    }
+  }
+}
